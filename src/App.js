543e53860f829b071df7754bae01c86e75c93faa
chickooooo <kshitijrajale@gmail.com>
2023-03-02 12:21:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Check from "./components/Check";
+
+function App() {
+  return (
+    <div className="App">
+      <Check />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Check.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Axios from "axios";
+import "./Check.css";
+
+function Check() {
+  // text content
+  const [text, setText] = useState("-");
+
+  // make api request
+  const fetchData = () => {
+    Axios.get("https://jsonplaceholder.typicode.com/todos/1").then(
+      (response) => {
+        setText(response.data["title"]);
+      }
+    );
+  };
+
+  return (
+    <div className="check">
+      <h1>Hola!</h1>
+      <button type="button" onClick={fetchData}>
+        Get data
+      </button>
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default Check;
